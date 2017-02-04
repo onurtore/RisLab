f3440e76c1059dca78bf4f9b4c4b4f4ddb8f569d
Onur Berk Töre <onurberk.tore@std.yeditepe.edu.tr>
2017-02-04 13:50:23 +0200

    Add files via upload

---
+++ PathFinder/main.cpp
@@ -0,0 +1,33 @@
+/**********************************************************************
+Onur Berk Töre
+Yeditepe University, RIS Lab
+
+
+Potantial Field Method for point, in 2D Environment, with wavefront algorithm
+
+**********************************************************************/
+
+
+#include <iostream>
+#include "wheretoGo.h"
+#include <conio.h>
+using namespace std;
+
+
+
+int main(){
+
+	wheretoGo myClass;
+	myClass.calculateWavefront();
+	
+
+	
+	//myClass.calculatePath();
+	myClass.printMatrix();
+	
+	cout << "The End\n";
+	
+	getch();
+	
+
+}
---
+++ PathFinder/wheretoGo.h
@@ -0,0 +1,112 @@
+/**********************************************************************
+Onur Berk Töre
+Yeditepe University, RIS Lab
+
+Potantial Field Method for point, in 2D Environment, with wavefront algorithm
+
+**********************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+using namespace std;
+
+
+class wheretoGo{
+
+public: 
+
+	wheretoGo();
+
+	~wheretoGo();
+
+	void initializeMatrix();
+	void addStartPoint(int x,int y);
+	void addTargetPoint(int x,int y);
+	void add4Wall();
+	void addBarrier(int x,int y);
+	void calculatePath();
+	int isInTarget();
+	
+	void calculateForce();
+	void calculateWavefront();
+	void calculateWavefrontPath();
+	vector<double> findMinPath(int x,int y);
+	int	minValue(double x, double y);
+
+
+
+	bool isStuck();
+	void saveMe();
+	
+	  
+	void fAtt();
+	void fRepel();
+	
+	double fattx;
+	double fatty;
+
+	double frepelx;
+	double frepely;
+	
+	double fnetx;
+	double fnety;
+
+	double pointMass;
+
+	double attractiveConstant;
+	double repulsiveConstant;
+	
+	double distanceOfInfluence;
+	/*
+	Dýstance  influence imposed  by obstacles; its value depends on the condition of
+	the obstacle and the goal point of the robot, and is usually less than half distances
+	between the obstacles or shortest length from the destination to the obstacles
+	*/
+
+	int borderOut();
+
+	void toGo();
+
+	void printMatrix();
+	vector<vector<vector<float> > > pathMatrix;
+
+	
+	double firstTime; 
+	
+	double targetRadius;
+	double pointRadius;
+
+	vector<double> targetsX;
+	vector<double> targetsY;
+
+	vector<double> barriersX;
+	vector<double> barriersY;
+
+	double targetCounter;
+	double currentTarget;
+	
+	double matrixHeight;
+	double matrixWidth;
+
+	double pointx;
+	double pointy;
+
+	double pointvelx;
+	double pointvely;
+
+	double targetx;
+	double targety;
+
+	double runs;
+
+	ofstream myfile1;
+	ofstream myfile2;
+	ofstream myfile3;
+	ofstream myfile4;
+
+	vector< pair<int, int>> neighborhood;
+
+};
